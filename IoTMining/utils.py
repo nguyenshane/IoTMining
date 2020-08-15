e163bf9c320e1f9ccdccfe74d91eb451c9008985
nguyenshane <nguyenshane@gmail.com>
2020-08-15 16:59:46 -0700

    Fixed comments

--- IoTMining/utils.py
+++ IoTMining/utils.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-timePartitionMap = {"0": {"start": 6, "end": 11},
-                    "1": {"start": 12, "end": 19},
-                    "2": {"start": 20, "end": 5}}
+timePartitionMap = {"0": {"start": 6, "end": 11}, # Morning
+                    "1": {"start": 12, "end": 19}, # Afternoon
+                    "2": {"start": 20, "end": 5}} # Everning
 
 def timeInRange(start, end, x):
     """Return true if x is in the range [start, end]"""
