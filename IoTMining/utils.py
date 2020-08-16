e20b2ee38052ad7bb4898efb25e9e8627325a4b3
qabach <69610671+qabach@users.noreply.github.com>
2020-08-16 15:27:07 -0700

    Update utils.py

--- IoTMining/utils.py
+++ IoTMining/utils.py
@@ -4,6 +4,93 @@ timePartitionMap = {"0": {"start": 6, "end": 11}, # Morning
                     "1": {"start": 12, "end": 19}, # Afternoon
                     "2": {"start": 20, "end": 5}} # Everning
 
+"""
+Organizing sensors IDs into sets.
+
+"""
+
+LivingRoomSensorSet =    ( "M001",
+                           "M002",
+                           "M003",
+                           "M004",
+                           "M005",
+                           "M006",
+                           "M007",
+                           "M008",
+                           "M009",
+                           "M010",
+                           "M011",
+                           "M012",
+                           "M013",
+                           "M014",
+                           "M015")
+R1roomSensorSet =     ("M044",
+                       "M045",
+                       "M046",
+                       "M047",
+                       "M048",
+                       "M049",
+                       "M050")
+
+R2roomSensorSet =    ("M030",
+                      "M031",
+                      "M032",
+                      "M033",
+                      "M034",
+                      "M035",
+                      "M036")
+
+
+upstairsHallSensorSet =   ("M027",
+                            "M028",
+                            "M029")
+
+downstairsHallSensorSet =    ("M021",
+                              "M022",
+                              "M023",
+                              "M024",
+                              "M025",
+                              "M026")
+
+kitchenSensorSet =   ("M016",
+                      "M017",
+                      "M018",
+                      "M051" )
+
+bathroomSensorSet =   ("M037",
+                       "M038",
+                       "M039",
+                       "M040",
+                       "M041")
+
+
+
+
+sensorGroupList = [  LivingRoomSensorSet,
+                     R1roomSensorSet,
+                     R2roomSensorSet,
+                     upstairsHallSensorSet,
+                     downstairsHallSensorSet,
+                     kitchenSensorSet,
+                     bathroomSensorSet]
+
+
+
+
+"""
+Function sensorFilter
+this funcntion will read in the sensor ID and return the primary ID of each group
+primary ID for each group is the frist element of that group
+"""
+def sensorFilter(sensorID):
+    for element in sensorGroupList:
+            if (element.count(sensorID) > 0):
+                return element[0]
+    return None #return None if not found in the group list
+
+
+
+
 def timeInRange(start, end, x):
     """Return true if x is in the range [start, end]"""
     if start <= end:
