d7ce65a449ab402bbfd9efaae9b1540c4a7ce4cc
qabach <69610671+qabach@users.noreply.github.com>
2020-08-19 14:34:09 -0700

    Update timePruning.py

--- IoTMining/timePruning.py
+++ IoTMining/timePruning.py
@@ -1,31 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 17 13:35:15 2020
+
+@author: QB
+"""
+
+
 from utils import labelSet
 from utils import timeStampDiff
 from utils import timePruningThreshold
+from utils import basketsKeySet
+from collections import defaultdict
 import numpy as np
 
 
-def timePruning():
-    """Return a list of routine items i.e. a basket for A-priori """
+filename = "./npy/datatestnpy.npy"
+
+def timePruning(filename):
+    """Return a map of routine items i.e. a baskets for A-priori"""
+    """keys of the map are the combinations of dayOfWeek + partitionTimeOfDay i.e. 00, 01, 02...62 """
     # an item is routine if its duration is longer then the time pruning threshold
     # a routine item can be occupancy: motion sensors ;and usage: lightning sensor
-    routineItems = []
-    dataTable = np.load("./npy/datatimeprunetestnpy.npy", allow_pickle = True)
-    
-    for elem in labelSet:
-        totalDuration = 0
+
+    routineItemsMap = defaultdict(list)
+    dataTable = np.load(filename, allow_pickle = True)
+
+    for key in basketsKeySet:
         timeStampList = []
-        endPoint = dataTable.shape
+        routineItems = []
+        for elem in labelSet: # scan the data for each sensor ID
+            totalDuration = 0
+            endPoint = dataTable.shape
+            lookingForOn = True # flag to alternate looking for ON and not ON (i.e. OFF)
+            for row in range(0,endPoint[0]):
+                keyInTable = str(dataTable[row][1])+str(dataTable[row][2])
+                if key == keyInTable:
+                    #in case reach the end but OFF not found (i.e. device has only ON)
+                    if row == (endPoint[0]-1) and not lookingForOn:
+                        timeStamp = str(dataTable[row][0]).split()
+                        timeStampList.append(timeStamp[1])
+                        
+                        
+                    #looking for ON
+                    if elem == dataTable[row][3] and lookingForOn and "ON" == str(dataTable[row][4]):
+                        timeStamp = str(dataTable[row][0]).split()
+                        timeStampList.append(timeStamp[1])
+                        lookingForOn = False
+                        
+                    #looking for OFF
+                    if elem == dataTable[row][3] and not lookingForOn and "OFF" == str(dataTable[row][4]):
+                        timeStamp = str(dataTable[row][0]).split()
+                        timeStampList.append(timeStamp[1])
+                        lookingForOn = True
+                        
+                    #compute the duration of the intervals between ON and OFF
+                    if len(timeStampList) == 2:
+                        totalDuration += timeStampDiff(timeStampList[0],timeStampList[1])
+                        timeStampList.clear()
         
-        for row in range(0,endPoint[0]):
-            if elem == dataTable[row][3]:
-                timeStamp = str(dataTable[row][0]).split()
-                timeStampList.append(timeStamp[1])
+            if totalDuration > timePruningThreshold : #time pruning
+                routineItems.append(elem)
                 
-        #compute the duration of the intervals between triggered for a sensorID
-        for i in range(0,len(timeStampList) - 1):
-            totalDuration += timeStampDiff(timeStampList[i],timeStampList[i+1])
-            
-        if totalDuration > timePruningThreshold :
-            routineItems.append(elem)
-            
-    return routineItems
+        routineItemsMap[key].append(routineItems)
+    return routineItemsMap
+
+# un-comment the line below to test output
+#print(timePruning(filename))
+
+
+
+
